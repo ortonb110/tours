63573576d0a0fb563d0e07cb159e28f8b41e1cb4
Orton Bright <ortonb110@gmail.com>
2023-05-03 23:21:22 +0000

    API Fetch Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
+import { useEffect, useState } from "react";
 import Tours from "./Components/Tours";
+import axios from "axios";
 
 
 function App() {
+  const [tourSites, setTourSites] = useState([])
+  
+  
+
+  const fetchData= () => {
+     axios.get('https://course-api.com/react-tours-project').then((response)=> {
+      setTourSites(response.data);
+    })
+    console.log(tourSites)
+  }
+ 
+  useEffect({
+    
+  }, [tourSites]);
+
   return (
     <main>
-      <Tours/>
+      <button onClick={fetchData}>Load</button>
+      <Tours props={tourSites}/>
     </main>
   );
 }
