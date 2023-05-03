0de9fa447197de3b0d869a113d3c91b68f918bf9
Orton Bright <ortonb110@gmail.com>
2023-05-03 23:50:56 +0000

    UI Design

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,37 @@
 import { useEffect, useState } from "react";
 import Tours from "./Components/Tours";
-import axios from "axios";
+
 
 
 function App() {
   const [tourSites, setTourSites] = useState([])
   
   
-
-  const fetchData= () => {
-     axios.get('https://course-api.com/react-tours-project').then((response)=> {
-      setTourSites(response.data);
-    })
-    console.log(tourSites)
+  const fetchData= async() => {
+    let tour;
+     try {
+      const sites =  await fetch("https://course-api.com/react-tours-project");
+      tour = await sites.json();
+      setTourSites(tour);
+     } catch (error) {
+      console.log(error);
+     }
+    console.log(tour);
   }
  
-  useEffect({
-    
-  }, [tourSites]);
+  useEffect(()=> {
+    fetchData();
+  }, [])
+ 
 
   return (
-    <main>
-      <button onClick={fetchData}>Load</button>
-      <Tours props={tourSites}/>
+    <main className="mx-[20rem]">
+      <h1 className="text-center">Our Tours</h1>
+      <section className="grid gap-2 grid-cols-3">
+      {tourSites.map((items)=> {
+        return <Tours key={items.id} items={items}/>
+      })}
+      </section>
     </main>
   );
 }
--- src/Components/Tours.jsx
+++ src/Components/Tours.jsx
@@ -1,14 +1,14 @@
-const Tours = (props) => {
+const Tours = ({items}) => {
   return (
-    <article className="rounded-md w-[36rem]">
-      <img src={props.image} alt="Tourist attraction" className="w-full " />
+    <article className="rounded-md w-[36rem] border-[1px] overflow-hidden">
+      <img src={items.image} alt="Tourist attraction" className="w-full " />
       <div className="px-[1.5rem] py-[2rem] text-center">
-        <h2 className="font-bold capitalize ">{props.name}</h2>
-        <p className="text-gray-500">{props.info}</p>
-      </div>
+        <h2 className="font-bold capitalize ">{items.name}</h2>
+        <p className="text-gray-500">{items.info}</p>
       <button className="w-full py-[0.2rem] text-green-500 border-[1px] border-green-500 hover:text-white hover:bg-green-500 transition-all ease-in-out duration-300 rounded-md">
         Not interested
       </button>
+      </div>
     </article>
   );
 };
