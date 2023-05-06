15a0fa1a8b15b24afca2210771203664120c9149
Orton Bright <ortonb110@gmail.com>
2023-05-06 18:41:20 +0000

    Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,43 @@
 import { useEffect, useState } from "react";
 import Tours from "./Components/Tours";
 
-
-
 function App() {
-  const [tourSites, setTourSites] = useState([])
-  
-  
-  const fetchData= async() => {
+  const [tourSites, setTourSites] = useState([]);
+
+  const fetchData = async () => {
     let tour;
-     try {
-      const sites =  await fetch("https://course-api.com/react-tours-project");
+    try {
+      const sites = await fetch("https://course-api.com/react-tours-project");
       tour = await sites.json();
       setTourSites(tour);
-     } catch (error) {
+    } catch (error) {
       console.log(error);
-     }
+    }
     console.log(tour);
-  }
- 
-  useEffect(()=> {
+  };
+
+  useEffect(() => {
     fetchData();
-  }, [])
- 
+  }, []);
+
+  const removeTour = (id)=> {
+    const newTours = tourSites.filter((tour) => tour.id !==id);
+    setTourSites(newTours);
+    const lengtfOfTours = tourSites.length;
+  }
 
   return (
-    <main className="mx-[20rem]">
-      <h1 className="text-center">Our Tours</h1>
-      <section className="grid gap-2 grid-cols-3">
-      {tourSites.map((items)=> {
-        return <Tours key={items.id} items={items}/>
-      })}
+    <main className="px-[5rem] py-[5rem]">
+      <h1 className=" text-[3rem] font-bold mb-[4rem] border-b-4 w-fit mx-auto border-green-500">
+        {
+          tourSites.length < 1 ? "No Tours" : "Our Tours"
+        }
+      </h1>
+     <button onClick={fetchData} className={`${tourSites.length < 1 ? "visible" : "hidden"} mx-auto px-[1rem] text-green-500 border-[1px] border-green-500 hover:text-white hover:bg-green-500 transition-all ease-in-out duration-300 rounded-md`} >Refresh</button>
+      <section className="grid grid-cols-3 mx-auto gap-6">
+        {tourSites.map((items) => {
+          return <Tours key={items.id} items={items} removeTour={removeTour}/>;
+        })}
       </section>
     </main>
   );
--- src/Components/Tours.jsx
+++ src/Components/Tours.jsx
@@ -1,13 +1,35 @@
-const Tours = ({items}) => {
+import { useState } from "react";
+
+const Tours = ({ items, removeTour }) => {
+  const [readMore, setReadMore] = useState(false);
+  const toggleReadMore = () => {
+    setReadMore((prevState) => !prevState);
+  };
+
   return (
-    <article className="rounded-md w-[36rem] border-[1px] overflow-hidden">
-      <img src={items.image} alt="Tourist attraction" className="w-full " />
+    <article className="rounded-md w-[50rem] border-[1px] overflow-hidden">
+      <img
+        src={items.image}
+        alt="Tourist attraction"
+        className="w-full h-[33.1rem] object-cover"
+      />
       <div className="px-[1.5rem] py-[2rem] text-center">
-        <h2 className="font-bold capitalize ">{items.name}</h2>
-        <p className="text-gray-500">{items.info}</p>
-      <button className="w-full py-[0.2rem] text-green-500 border-[1px] border-green-500 hover:text-white hover:bg-green-500 transition-all ease-in-out duration-300 rounded-md">
-        Not interested
-      </button>
+        <h2 className="font-bold capitalize mb-[1rem]">{items.name}</h2>
+        <p className="text-gray-500 transition-all ease-in-out duration-300 mb-[1rem]">
+          {readMore ? items.info : `${items.info.substring(0, 200)}...`}
+          <button
+            className="ml-[1rem] text-green-500 border-[1px] rounded-md px-[1rem] hover:text-white hover:bg-green-500 transition-all ease-in-out duration-300"
+            onClick={toggleReadMore}
+          >
+            {readMore ? "Show less" : "Read more"}
+          </button>
+        </p>
+        <button
+          onClick={()=>{removeTour(items.id)}}
+          className="w-full py-[0.2rem] text-green-500 border-[1px] border-green-500 hover:text-white hover:bg-green-500 transition-all ease-in-out duration-300 rounded-md"
+        >
+          Not interested
+        </button>
       </div>
     </article>
   );
